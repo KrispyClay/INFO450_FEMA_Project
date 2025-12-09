4233f63854e2150e95400afb87a8d71eff49924e
Claytis Egbulem <112145069+KrispyClay@users.noreply.github.com>
2025-12-09 00:45:50 -0500

    Add files via upload

---
+++ fema_app.py
@@ -0,0 +1,17 @@
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("fema_sample.csv")
+
+st.title("FEMA Disaster Relief Dashboard")
+st.write("Authors: YOUR NAME")
+
+st.subheader("Histogram of Repair Amount")
+fig1 = px.histogram(df, x="repairAmount", nbins=40)
+st.plotly_chart(fig1)
+
+st.subheader("Boxplot: Repair Amount by TSA Eligibility")
+fig2 = px.box(df, x="tsaEligible", y="repairAmount")
+st.plotly_chart(fig2)
