481e9a9aad9243820e2e94552e8872d014709810
Claytis Egbulem <112145069+KrispyClay@users.noreply.github.com>
2025-12-09 00:47:01 -0500

    Update fema_app.py

--- fema_app.py
+++ fema_app.py
@@ -3,10 +3,10 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv("fema_sample.csv")
+df = pd.read_csv("fema_small.csv")
 
 st.title("FEMA Disaster Relief Dashboard")
-st.write("Authors: YOUR NAME")
+st.write("Creator: Claytis Egbulem")
 
 st.subheader("Histogram of Repair Amount")
 fig1 = px.histogram(df, x="repairAmount", nbins=40)
@@ -15,3 +15,4 @@ st.plotly_chart(fig1)
 st.subheader("Boxplot: Repair Amount by TSA Eligibility")
 fig2 = px.box(df, x="tsaEligible", y="repairAmount")
 st.plotly_chart(fig2)
+
